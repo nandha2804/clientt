819e6c592458667bf9b7df50bce343c26c30901c
Nandhakumar <143866475+nandha2804@users.noreply.github.com>
2024-10-31 08:58:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/clientt/', // Ensure this is correct for GitHub Pages
+  base: '/clientt/',
   build: {
-    outDir: 'dist', // Ensure this is set to 'dist'
+    outDir: 'dist',
+  },
+  define: {
+    'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL)
   }
 });
